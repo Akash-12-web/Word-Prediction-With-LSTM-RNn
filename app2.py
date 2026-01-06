10a3fcc8325fb0f5a3e44bcc54fc910e23ba8d59
Akash <akashpandharpurkar777@gmail.com>
2026-01-06 14:45:56 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+
+#load the lstm model
+model = load_model('next_word_lstm.h5')
+
+#load tokenizer
+with open('tokenizer.pkl','rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+def predict_next_word(model,tokenizer,text,max_sequence_len):
+  token_list = tokenizer.texts_to_sequences([text])[0]
+  if len(token_list) >= max_sequence_len:
+    token_list = token_list[-(max_sequence_len):] 
+  token_list = pad_sequences([token_list],max_sequence_len-1,padding='pre')
+  predicted = model.predict(token_list,verbose=0)
+  predicted_word_index = np.argmax(predicted,axis=1)
+  for word,index in tokenizer.word_index.items():
+    if index == predicted_word_index:
+      return word
+  return None
+
+#streamlit app
+st.title("Next Word Prediction With LSTM And Early Stopping")
+
+input_text = st.text_input("Enter the sequence of words")
+if st.button("predict Next word"):
+  max_sequence_len = model.input_shape[1]+1
+  next_word = predict_next_word(model,tokenizer,input_text,max_sequence_len)
+  st.write(f"Next word: {next_word}")
